975e1f424524083e26fa0d1e3d33e0c54b366370
Mina Samir <MenaSamerCap@gmail.com>
2020-02-27 19:55:01 +0200

    handled all events from the state bloc

--- lib/bloc/weather_bloc.dart
+++ lib/bloc/weather_bloc.dart
@@ -3,11 +3,16 @@ import 'dart:async';
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:weater_repo/data/model/weather.dart';
+import 'package:weater_repo/data/weather_repository.dart';
 
 part 'weather_event.dart';
 part 'weather_state.dart';
 
 class WeatherBloc extends Bloc<WeatherEvent, WeatherState> {
+  final WeatherRepository weatherRepository;
+
+  WeatherBloc(this.weatherRepository);
+
   @override
   WeatherState get initialState => WeatherInitial();
 
@@ -15,6 +20,24 @@ class WeatherBloc extends Bloc<WeatherEvent, WeatherState> {
   Stream<WeatherState> mapEventToState(
     WeatherEvent event,
   ) async* {
-    // TODO: implement mapEventToState
+    yield WeatherLoading();
+    if (event is GetWether) {
+      try {
+        final weather = await weatherRepository.fetchWeather(event.cityName);
+        yield WeatherLoaded(weather);
+      } on NetworkError {
+        // we can provide integer codes that will provide us with translation to the ui
+        yield WeatherError("Could not featch weather. Is the devide online?");
+      }
+    } else if (event is GetDetailsWether) {
+      try {
+        final weather =
+            await weatherRepository.fetchDetailedWeather(event.cityName);
+        yield WeatherLoaded(weather);
+      } on NetworkError {
+        // we can provide integer codes that will provide us with translation to the ui
+        yield WeatherError("Could not featch weather. Is the devide online?");
+      }
+    }
   }
 }
